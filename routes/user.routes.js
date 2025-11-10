0dfce123c5328cf74ef3bc80adb1eb1dd2b156a2
aniTech-addict <kirti7666anand15@gmail.com>
2025-11-10 12:01:47 +0530

    feat(routes): add user routes

---
+++ routes/user.routes.js
@@ -0,0 +1,24 @@
+import { Router } from 'express';
+
+userRouter = Router();
+
+userRouter.get('/', (req, res) => {
+    res.send('get All user details');
+});
+
+userRouter.get('/:id', (req, res) => {
+    res.send('Get user details');
+});
+
+userRouter.post('/', (req, res) => {
+    res.send('Create new User');
+});
+
+userRouter.put('/:id', (req, res) => {
+    res.send('Update user details');
+});
+userRouter.delete('/:id', (req, res) => {
+    res.send('Delete user details');
+});
+
+export default userRouter;
