da22e4fe67f92bdc6af48f4a6c867d9d1d716df1
aniTech-addict <kirti7666anand15@gmail.com>
2025-11-17 01:27:18 +0530

    refactor(upload): streamline upload routes and remove unused multer integration

--- routes/upload.routes.js
+++ routes/upload.routes.js
@@ -1,17 +1,8 @@
-import { Router } from 'express';
-import { uploadAvatar } from '../controller/user.controller.js';
-// Posts
-import { uploadPost } from '../controller/post.controller.js';
-import multerCore from '../services/upload/multerCore.service.js';
-
-const upload = multerCore.getUploader('uploads/posts/images');
+import { Router} from 'express';
+import { createPost } from '../controller/post.controller.js';
 
 const uploadRouter = Router();
 
-uploadRouter.get('/', (req, res) => {
-    res.send('Upload Route');
-});
-uploadRouter.post('/profile', upload.single('avatar'), uploadAvatar);
-uploadRouter.post('/post', upload.single('avatar'), uploadPost);
+uploadRouter.post('/post', createPost);
 
-export default uploadRouter;
+export default uploadRouter
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,17 +2,15 @@ import { Router } from 'express';
 import { getUser, getUsers, uploadAvatar } from '../controller/user.controller.js';
 import authorizationMiddleware from '../middleware/authorization.middleware.js';
 import { authorizeAdmin } from '../middleware/authorizeAdmin.middleware.js';
-import multerCore from '../services/upload/multerCore.service.js';
-
-const upload = multerCore.getUploader('uploads/profile');
+import upload from '../config/multer.config.js';
 
 const userRouter = Router();
 
 userRouter.get('/', authorizationMiddleware, authorizeAdmin, getUsers);
 
 userRouter.get('/:id', authorizationMiddleware, getUser);
-
-userRouter.post('/avatar', authorizationMiddleware, upload.single('avatar'), uploadAvatar);
+console.log('reachedHere');
+userRouter.post('/avatar', authorizationMiddleware, upload.single('avatar'),  uploadAvatar);
 
 userRouter.post('/', (req, res) => {
     res.send('Create new User');
